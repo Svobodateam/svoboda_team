2c81d727f558c59a6b4cbc515ab1bfff18b9c749
Vlad Chobanitsa <Chobanitsavlad@gmail.com>
2021-12-22 00:27:07 +0200

    minor refactoring

--- Svoboda_team/mainwindow.cpp
+++ Svoboda_team/mainwindow.cpp
@@ -4,17 +4,15 @@
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     m_dbManager(new DBManager()),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow) {
+
     ui->setupUi(this);
-      
     setupMenuBar();
 }
 
-void MainWindow::setupMenuBar()
-{
-    actionGroup = new QActionGroup(this);
+void MainWindow::setupMenuBar() {
 
+    actionGroup = new QActionGroup(this);
     actionGroup->setExclusive(true);
     ui->menuTheme->actions().first()->setChecked(true);
     for (int i = 0; i < ui->menuTheme->actions().size(); ++i) {
@@ -22,26 +20,26 @@ void MainWindow::setupMenuBar()
     }
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
+    delete m_dbManager;
     delete ui;
 }
 
-void MainWindow::on_actionDiffnes_triggered()
-{
+void MainWindow::on_actionDiffnes_triggered() {
     QFile file(":/Diffnes.qss");
     file.open(QFile::ReadOnly);
     QString styleSheet = QLatin1String(file.readAll());
 
     ui->widget->setStyleSheet(styleSheet);
+    file.close();
 }
 
-void MainWindow::on_actionMedize_triggered()
-{
+void MainWindow::on_actionMedize_triggered() {
     QFile file(":/Medize.qss");
     file.open(QFile::ReadOnly);
     QString styleSheet = QLatin1String(file.readAll());
 
     ui->widget->setStyleSheet(styleSheet);
+    file.close();
 }
 
