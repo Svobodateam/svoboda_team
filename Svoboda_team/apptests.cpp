289f2038266fb915a33639ab14f8e34d87466a15
RockheimPain <didykanton27@gmail.com>
2021-12-22 22:32:57 +0200

    adding test - st-58

--- Svoboda_team/apptests.cpp
+++ Svoboda_team/apptests.cpp
@@ -22,3 +22,45 @@ void AppTests::userFieldsTest() {
     QCOMPARE(m_dbManager->getCurrentUser()->lastName, "Ivanov");
     QCOMPARE(m_dbManager->getCurrentUser()->role, "designer");
 }
+
+void AppTests::setCurrentUserTest()
+{
+    QMap<LeaveType, int> tmpLeave;
+    tmpLeave[LeaveType::Vacation] = 21;
+    tmpLeave[LeaveType::UnpaidVacation] = 15;
+    tmpLeave[LeaveType::Sickness75] = 10;
+    tmpLeave[LeaveType::Sickness100] = 5;
+
+    User *tmpUser = new User;
+    tmpUser->id = 1;
+    tmpUser->leavesCount = tmpLeave;
+    tmpUser->userName = "usergoga";
+    tmpUser->firstName = "Goga";
+    tmpUser->lastName = "Tuputia";
+    tmpUser->role = "Juggernaut";
+
+    m_dbManager->setCurrentUser(tmpUser);
+    QCOMPARE(m_dbManager->getCurrentUser()->userName, "usergoga");
+    QCOMPARE(m_dbManager->getCurrentUser()->firstName, "Goga");
+    QCOMPARE(m_dbManager->getCurrentUser()->lastName, "Tuputia");
+    QCOMPARE(m_dbManager->getCurrentUser()->role, "Juggernaut");
+}
+
+void AppTests::updateLeaveCountTest()
+{
+    m_dbManager->authorizeUser("userivan");
+    bool tmp = m_dbManager->updateLeaveCount(LeaveType::Vacation, 9);
+    QVERIFY(tmp);
+}
+
+void AppTests::retrieveLeaveCountTest()
+{
+    m_dbManager->authorizeUser("userivan");
+    QMap<LeaveType, int> tmpLeave = m_dbManager->retrieveLeaveCount();
+
+    QCOMPARE(m_dbManager->getCurrentUser()->userName, "userivan");
+    QCOMPARE(m_dbManager->getCurrentUser()->firstName, "Ivan");
+    QCOMPARE(m_dbManager->getCurrentUser()->lastName, "Ivanov");
+    QCOMPARE(m_dbManager->getCurrentUser()->role, "designer");
+}
+
