9ee6ce6d4931150886359714165aa3899dec2346
illumlg <47866807+illumlg@users.noreply.github.com>
2021-12-22 21:03:49 +0200

    st-56 create user fields test (#36)

--- Svoboda_team/apptests.cpp
+++ Svoboda_team/apptests.cpp
@@ -14,3 +14,11 @@ void AppTests::authTest() {
     bool auth = m_dbManager->authorizeUser("userivan");
     QVERIFY(auth);
 }
+
+void AppTests::userFieldsTest() {
+    m_dbManager->authorizeUser("userivan");
+    QCOMPARE(m_dbManager->getCurrentUser()->userName, "userivan");
+    QCOMPARE(m_dbManager->getCurrentUser()->firstName, "Ivan");
+    QCOMPARE(m_dbManager->getCurrentUser()->lastName, "Ivanov");
+    QCOMPARE(m_dbManager->getCurrentUser()->role, "designer");
+}
--- Svoboda_team/apptests.h
+++ Svoboda_team/apptests.h
@@ -14,6 +14,7 @@ private:
     DBManager* m_dbManager;
 private slots:
     void authTest();
+    void userFieldsTest();
 };
 
 #endif // APPTESTS_H
